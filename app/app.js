8425d2014064c70215ed98f6200b0fb5e80b53f2
Tech5818 <pqow5818@gmail.com>
2023-08-18 08:28:57 +0900

    route change

--- app/app.js
+++ app/app.js
@@ -3,12 +3,12 @@ const express = require("express");
 const app = express();
 
 //라우팅
-const home = require("./routes/home");
+const home = require("./src/routes/home");
 
 app.use("/", home)
 
 //앱 세팅
-app.set("views", "./views");
+app.set("views", "./src/views");
 app.set("view engine", "ejs");
 
 module.exports = app;

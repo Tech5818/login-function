c48273b98a8c3e46ec9410fd7d494447f2ff8480
Tech5818 <pqow5818@gmail.com>
2023-08-19 17:16:29 +0900

    User객체 구현

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -5,7 +5,7 @@ class UserStorage {
         name: ["권호","정욱","지상"]
     };
     
-    static getUsers(...fields) {
+    static getUsers (...fields) {
         const users = this.#users;
         const newUsers = fields.reduce((newUsers,field) => {
             if (users.hasOwnProperty(field)) {
@@ -14,6 +14,18 @@ class UserStorage {
             return newUsers
         }, {})
         return newUsers;
+    }
+
+    static getUserInfo(id) {
+        const users = this.#users;
+        const idx = users.id.indexOf(id);
+        const userKeys = Object.keys(users);
+        const userInfo = userKeys.reduce((newUser, info) => {
+            newUser[info] = users[info][idx];
+            return newUser
+        },{})
+
+        return userInfo;
     }
 }
 
--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,4 +1,4 @@
-const UserStorage = require("../../models/UserStorage");
+const User = require("../../models/User");
 
 const output = {
     home : (req, res) => {
@@ -11,25 +11,28 @@ const output = {
 
 const process = {
     login: (req, res) => {
-        const id = req.body.id;
-        const password = req.body.password;
+        const user = new User(req,body);
+        const response = user.login();
+        return res.json(response);
+        // const id = req.body.id;
+        // const password = req.body.password;
 
-        const users = UserStorage.getUsers("id", "password")
+        // const users = UserStorage.getUsers("id", "password")
         
-        const response = {};
+        // const response = {};
         
-        if (users.id.includes(id)) {
-            const idx = users.id.indexOf(id);
+        // if (users.id.includes(id)) {
+        //     const idx = users.id.indexOf(id);
 
-            if (users.password[idx] === password) {
-                response.success = true;
-                return res.json(response)
-            }
-        }
+        //     if (users.password[idx] === password) {
+        //         response.success = true;
+        //         return res.json(response)
+        //     }
+        // }
 
-        response.success = false;
-        response.msg = "로그인에 실패하셨습니다."
-        return res.json(response)
+        // response.success = false;
+        // response.msg = "로그인에 실패하셨습니다."
+        // return res.json(response)
     },
 }
 

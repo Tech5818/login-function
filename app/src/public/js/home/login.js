325c91fbc6494fd57e84252e62ad4c575d5b8565
Tech5818 <pqow5818@gmail.com>
2023-08-19 13:52:31 +0900

    add fetch

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -7,7 +7,14 @@ const login = () => {
         id : id.value,
         password : password.value
     };
-    console.log(req)
+    console.log(req);
+    fetch("/login", {
+        method : "POST",
+        headers: {
+            "Content-Type" : "application/json",
+        },
+        body : JSON.stringify(req)
+    })
 }
 
 loginBtn.addEventListener("click", login);

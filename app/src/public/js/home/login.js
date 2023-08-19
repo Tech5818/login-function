4b34d2cce28e0b53de6f218cf6f4abe5a4f47c65
Tech5818 <pqow5818@gmail.com>
2023-08-19 13:44:22 +0900

    login function in front

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,2 +1,14 @@
-console.log("hello");
-console.log("2");
+const id = document.querySelector("#id");
+const password = document.querySelector("#password");
+const loginBtn = document.querySelector("#loginBtn");
+
+const login = () => {
+    const req = {
+        id : id.value,
+        password : password.value
+    };
+    console.log(req)
+}
+
+loginBtn.addEventListener("click", login);
+
